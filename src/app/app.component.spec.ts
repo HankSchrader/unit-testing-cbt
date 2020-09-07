62b22f7e8ae1b5815df4379aa03efc58e6f84ed8
Erik Mikac <mikacew@gmail.com>
2020-09-06 23:28:07 -0400

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,24 @@
-import { TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let appFixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
     }).compileComponents();
+    appFixture = TestBed.createComponent(AppComponent);
+    component = appFixture.componentInstance;
+    appFixture.detectChanges();
+  });
+
+
+  beforeEach(() => {
+
   });
 
   it('should create the app', () => {
@@ -27,5 +38,9 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('.content span').textContent).toContain('unit-testing-cbt app is running!');
+  });
+
+  it('should calculate the total and add sales tax.', () => {
+    expect(component.calculateTotalWithTax(1.06, 4, 4, 4)).toBe(12.72);
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'unit-testing-cbt';
+  item1 = '3';
+  item2 = '4';
+  item3 = '5';
+  totalPrice = 0;
+  shoppingCart = this.fb.group({
+    item1: [this.item1, Validators.pattern('^[0-9]*$')],
+    item2: [this.item2, Validators.pattern('^[0-9]*$')],
+    item3: [this.item3, Validators.pattern('^[0-9]*$')]
+  });
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+       this.convertToNumbersAndCalculate();
+    }
+
+  // tslint:disable-next-line:typedef
+  convertToNumbersAndCalculate() {
+    const i1 = Number.parseInt(this.item1);
+    const i2 = Number.parseInt(this.item2);
+    const i3 = Number.parseInt(this.item3);
+    this.totalPrice = this.calculateTotalWithTax(1.06, i1, i2, i3);
+  }
+  calculateTotalWithTax(taxRate, ...itemPrices): number {
+    return itemPrices.reduce((a, b) => {
+      return a + b;
+    }) * taxRate;
+  }
+
+  showItem() {
+    console.log(this.shoppingCart.controls);
+  }
 }
